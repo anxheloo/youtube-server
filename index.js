92ce68bc2e8ec4d5b1ff11d25c89c8c5e1478f1a
Anxhelo Cenollari <anxhelocenollari@gmail.com>
2024-01-12 15:46:02 +0100

    using origin from manual deploy cuz from github not working

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(
     // origin: "http://192.168.1.236:3000",
     // origin: "http://localhost:3000",
     // origin: "http://172.30.160.1:3000",
-    origin: "https://fabulous-blini-23e559.netlify.app",
+    origin: "https://dazzling-stardust-3ba042.netlify.app/",
     // credentials: true, // Set to true to pass the header, otherwise it is omitted.
     // exposedHeaders: ["Set-Cookie"],
 
